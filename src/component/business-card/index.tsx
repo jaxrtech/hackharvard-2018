0f54da7351569ddd4552d79009e017b63244032c
Paolo Ratti Tamayo <prattita@hawk.iit.edu>
2018-10-20 15:13:12 -0400

    Moved business-card to another folder

--- src/component/business-card/index.tsx
+++ src/component/business-card/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Icon, Card, Elevation } from '@blueprintjs/core';
 
 import { Business } from 'src/models';
+import "./style.css";
 
 export type BusinessCardProps = { model: Business; };
 
@@ -24,10 +25,10 @@ export class BusinessCard extends React.Component<BusinessCardProps> {
 
     return (
       <Card interactive={true} elevation={Elevation.TWO}>
-        <h5><a href="#">{name}</a></h5>
-        <span>{pricingDisplay}</span>
-        <span>{distance} mi</span>
-        <span>{stars}</span>
+        <h5><a className="businessName" href="#">{name}</a></h5>
+        <span className="businessPricing">{pricingDisplay}</span>
+        <span className="businessDistance">{distance} mi</span>
+        <span className="businessStars">{stars}</span>
       </Card>
     );
   }
