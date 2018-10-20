ec92efa5ba7c630d8c243380ff0f4cf344d4bb4d
Paolo Ratti Tamayo <prattita@hawk.iit.edu>
2018-10-20 13:34:31 -0400

    Removed unnesary module

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,38 +8,9 @@ import { Grid,  Row, Col } from 'react-flexbox-grid';
 
 import "./styles.css";
 
-type WeatherWidgetProps = { lat: number, long: number };
-
-@observer
-export class WeatherWidget extends React.Component<WeatherWidgetProps> {
-
-  private api = new WeatherService();
-
-  public componentDidMount() {
-    const { lat, long } = this.props;
-    this.api.update(lat, long);
-  }
-
-  public render() {
-    if (!this.api.ready) { return <p>Loading...</p>; }
-
-    const { location, forecast } = this.api.current;
-
-    return (
-      <>
-        <h2>{location.city}, {location.state}</h2>
-        <h3>{forecast.temperature} &deg;{forecast.temperatureUnit}</h3>
-        <h4>{forecast.shortForecast}</h4>
-      </>
-    );
-  }
-}
-
 @inject('config')
 @observer
 export class HomePage extends React.Component<{ config: ConfigStore }> {
-
-  
 
   constructor(props: any) {
     super(props);
