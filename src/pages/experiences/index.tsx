d81bc12de7902ca0718bab5a27ec8fb55a0dda86
Paolo Ratti Tamayo <prattita@hawk.iit.edu>
2018-10-20 22:41:03 -0400

    Playing with json

--- src/pages/experiences/index.tsx
+++ src/pages/experiences/index.tsx
@@ -1,49 +1,13 @@
 import * as React from 'react';
-import { render } from 'react-dom';
 
 export class ExperiencesPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tables: []
-        };
-    }
-
-    handleAddRow = () => {
-        this.setState((prevState, props) => {
-        const table = { content: "hello this is a new row!" };
-            return { tables: [...prevState.tables, table] };
-        });
-    };
-
-    handleRemoveRow = () => {
-        this.setState((prevState, props) => {
-            return { rows: prevState.rows.slice(1) };
-        });
-    };
-
     public render() {
         return (
             <>
-                <div>
-                    <table>
-                    <tbody>
-                        {this.state.tables.map(table => (
-                        <tr>
-                            <td>{row.content}</td>
-                        </tr>
-                        ))}
-                        <tr>
-                        <td className="" onClick={this.handleAddRow}>
-                            (+)
-                        </td>
-                        {Boolean(this.state.tables.length) && (
-                            <td onClick={this.handleRemoveRow}>(-)</td>
-                        )}
-                        </tr>
-                    </tbody>
-                    </table>
-                </div>
+                <h1>Experiences</h1>
+                <h2>Things to do this Weekend!</h2>
+
+
             </>
         );
     }
